3db19f3b42fd41b7058776d2f55037e572299d09
Victoria LePore <Tori@macbook-pro.local>
2018-03-17 11:51:39 -0400

    Initial Commit

---
+++ Weather Gift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Gift App
+//
+//  Created by Victoria LePore on 3/17/18.
+//  Copyright © 2018 Victoria LePore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
