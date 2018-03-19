8ca17ba68a714d7e118fa4bfbc8e5c949e454b1b
Victoria LePore <Tori@macbook-pro.local>
2018-03-19 12:17:56 -0400

    Weather Gift 7.1- 7.7

---
+++ Weather Gift App/DetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailVC.swift
+//  Weather Gift App
+//
+//  Created by Victoria LePore on 3/17/18.
+//  Copyright © 2018 Victoria LePore. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationLabel.text = locationsArray[currentPage]
+    }
+    
+
+}
